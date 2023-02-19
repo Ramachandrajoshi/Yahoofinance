262a6419ee2958e806ce8c215ed32e12f92beeff
Ramachandra joshi <devcontractor_rj@bybox.com>
2023-02-19 16:33:08 +0530

    responsive trade info

--- Vue_frontEnd/src/components/StockDetails.vue
+++ Vue_frontEnd/src/components/StockDetails.vue
@@ -6,29 +6,27 @@ const stock = useStockStore();
     <div class="mb-12">
         <h1 class="V-mt-4">Stock Details</h1>
         <v-row>
-            <v-col class="text-right">
+            <v-col cols="6" lg="3" class="text-right">
                 Company:
             </v-col>
-            <v-col class="text-h6">
+            <v-col cols="6" lg="3" class="text-h6">
                 {{ stock.stockData.quote?.longName }}
             </v-col>
-            <v-responsive width="100%"></v-responsive>
-            <v-col class="text-right">
+            <v-col cols="6" lg="3" class="text-right">
                 Type:
             </v-col>
-            <v-col class="text-h6">
+            <v-col cols="6" lg="3" class="text-h6">
                 {{ stock.stockData.quote?.typeDisp }}
             </v-col>
         </v-row>
         <v-row>
-            <v-col class="text-right">
+            <v-col cols="6" lg="3" class="text-right">
                 Symbol:
             </v-col>
-            <v-col class="text-h6">
+            <v-col cols="6" lg="3" class="text-h6">
                 {{ stock.stockData.quote?.symbol }}
             </v-col>
-            <v-responsive width="100%"></v-responsive>
-            <v-col class="text-right">
+            <v-col cols="6" lg="3" class="text-right">
                 Day Range (low-high):
             </v-col>
             <v-col class="text-h6">
@@ -36,29 +34,27 @@ const stock = useStockStore();
             </v-col>
         </v-row>
         <v-row>
-            <v-col class="text-right">
+            <v-col cols="6" lg="3" class="text-right">
                 Current Status:
             </v-col>
-            <v-col class="text-h6">
+            <v-col cols="6" lg="3" class="text-h6">
                 {{ stock.stockData.quote?.marketState }}
             </v-col>
-            <v-responsive width="100%"></v-responsive>
-            <v-col class="text-right">
+            <v-col cols="6" lg="3" class="text-right">
                 Exchange Timezone:
             </v-col>
-            <v-col class="text-h6">
+            <v-col cols="6" lg="3" class="text-h6">
                 {{ stock.stockData.quote?.exchangeTimezoneName }}
             </v-col>
         </v-row>
         <v-row>
-            <v-col class="text-right">
+            <v-col cols="6" lg="3" class="text-right">
                 Currency:
             </v-col>
             <v-col class="text-h6">
                 {{ stock.stockData.quote?.financialCurrency }}
             </v-col>
-            <v-responsive width="100%"></v-responsive>
-            <v-col class="text-right">
+            <v-col cols="6" lg="3" class="text-right">
                 52 Week Range (low-high):
             </v-col>
             <v-col class="text-h6">
